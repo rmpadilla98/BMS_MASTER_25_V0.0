513159d6ddd62c3541fb48724510b27324e041a9
rmpadilla98 <padillarodenasjose@gmail.com>
2025-12-30 20:03:51 +0100

    t

--- Scripts/Scripts/kicad_diff.sh
+++ Scripts/Scripts/kicad_diff.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# --- KiCad PCB Diff Script ---
-# Uso:
-#   ./Scripts/kicad_diff.sh <commit_old> <commit_new> <ruta_pcb>
-#
-# Ejemplo:
-#   ./Scripts/kicad_diff.sh HEAD~1 HEAD Hardware/Placa.kicad_pcb
-
 if [ "$#" -ne 3 ]; then
     echo "Uso: $0 <commit_old> <commit_new> <ruta_pcb>"
     exit 1
@@ -21,17 +14,22 @@ mkdir -p "$OUTDIR"
 
 echo "Generando diff entre $OLD y $NEW para $PCB..."
 
-# Exportar PCB antiguo
 git show "$OLD:$PCB" > "$OUTDIR/old.kicad_pcb"
-
-# Exportar PCB nuevo
 git show "$NEW:$PCB" > "$OUTDIR/new.kicad_pcb"
 
-# Convertir a SVG usando KiCad CLI
-kicad-cli pcb export svg "$OUTDIR/old.kicad_pcb" --output "$OUTDIR/old.svg"
-kicad-cli pcb export svg "$OUTDIR/new.kicad_pcb" --output "$OUTDIR/new.svg"
+# Exportar a SVG indicando capas (KiCad 9 requiere esto)
+LAYERS="F.Cu,B.Cu,F.SilkS,B.SilkS,Edge.Cuts"
 
-# Generar diff visual con ImageMagick
+kicad-cli pcb export svg "$OUTDIR/old.kicad_pcb" \
+  --layers $LAYERS \
+  --output "$OUTDIR/old.svg"
+
+kicad-cli pcb export svg "$OUTDIR/new.kicad_pcb" \
+  --layers $LAYERS \
+  --output "$OUTDIR/new.svg"
+
+# Generar diff visual
 compare "$OUTDIR/old.svg" "$OUTDIR/new.svg" "$OUTDIR/diff.png"
 
 echo "Diff generado en: $OUTDIR"
+
